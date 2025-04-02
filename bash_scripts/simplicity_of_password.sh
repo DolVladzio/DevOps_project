6561a1cfb779bc60c4f68faaa45bd67600c8d1c5
Vladzio <dolynkavladzio@gmail.com>
2025-04-02 16:48:42 +0000

    Added files to verify paasswords and count services

---
+++ bash_scripts/simplicity_of_password.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+####################################################################
+main_file="../file_for_analyze"
+admin_file="admin_passwords.txt"
+db_file="db_passwords.txt"
+
+lineFunc() {
+        echo "-----------------------------"
+}
+passInfo() {
+        echo "- $1 passwords"
+}
+# ADMIN PASSWORDS ##################################################
+lineFunc
+# Getting pass and delete everything useless
+grep "name=\"admin_pass\"" $main_file | sed -E 's/.*value="([^"]+)".*/\1/' | sort -u > $admin_file
+
+passInfo "Admin"
+
+# Checking pass
+while IFS= read -r line; do
+        echo $line | cracklib-check
+done < $admin_file
+
+lineFunc
+# DB PASSWORDS #####################################################
+# Getting pass and delete everything useless
+grep "softdbpass" $main_file | sed -E "s/.*'softdbpass' => '([^']+)'.*/\1/" | sort -u > $db_file
+
+passInfo "DB"
+
+# Checking pass
+while IFS= read -r line; do
+        echo $line | cracklib-check
+done < $db_file
+
+lineFunc
+####################################################################
