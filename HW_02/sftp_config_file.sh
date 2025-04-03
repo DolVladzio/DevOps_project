0a1e0c6caa80d054cbd500f14474e699d9f5c0be
Vladzio <dolynkavladzio@gmail.com>
2025-04-03 15:35:55 +0000

    Updated config

--- HW_02/sftp_config_file.sh
+++ HW_02/sftp_config_file.sh
@@ -7,13 +7,18 @@ echo "sftpuser:password" | sudo chpasswd
 
 echo "
 Match User sftpuser
-	ForceCommand internal-sftp
-	ChrootDirectory /home/sftpuser
-	AllowTCPForwarding no
-	X11Forwarding no
-	PermitTunnel no
-	PermitTTY no
+    ForceCommand internal-sftp
+    ChrootDirectory /home/sftpuser
+    AllowTCPForwarding no
+    X11Forwarding no
+    PermitTunnel no
+    PermitTTY no
 " | sudo tee -a /etc/ssh/sshd_config
 
 sudo chown root:root /home/sftpuser && sudo chmod 755 /home/sftpuser
+
 sudo mkdir -p /home/sftpuser/upload && sudo chown sftpuser:sftpuser /home/sftpuser/upload
+
+sudo sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+
+sudo systemctl restart ssh
