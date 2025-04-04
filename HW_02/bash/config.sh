aa98cfb6e547da2085a626704609ce2f2d0808d9
Vladzio <dolynkavladzio@gmail.com>
2025-04-04 17:19:12 +0000

    Updated code

--- HW_02/bash/config.sh
+++ HW_02/bash/config.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 ############################################################
 # Install OpenSSH
-sudo apt update && sudo apt install -y openssh-server
+sudo apt update && \
+	sudo apt install -y openssh-server
 
 # Create the SFTP user if not exists
 id -u sftpuser &>/dev/null || useradd -m -s /bin/bash sftpuser
@@ -12,10 +13,14 @@ chmod 700 /home/sftpuser/.ssh
 
 # Add public key from shared folder
 if [[ -f /vagrant/id_rsa.pub ]]; then
-	cat /vagrant/id_rsa.pub >> /home/sftpuser/.ssh/authorized_keys
+	cp /vagrant/ssh/id_rsa ~/.ssh/id_rsa && chmod 600 ~/.ssh/id_rsa
+	cat /vagrant/ssh/id_rsa.pub >> /home/sftpuser/.ssh/authorized_keys
 	chmod 600 /home/sftpuser/.ssh/authorized_keys
 	chown -R sftpuser:sftpuser /home/sftpuser/.ssh
 else
 	echo "Public key not found at /vagrant/id_rsa.pub"
 fi
+############################################################
+# Configure to launch the file every 5m
+
 ############################################################
