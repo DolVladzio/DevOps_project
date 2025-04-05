9ca3915d85796643ac33e6db0fa07b0822cd7de2
Vladzio <dolynkavladzio@gmail.com>
2025-04-05 10:20:43 +0000

    Updated code

---
+++ HW_02/bash/sent_logs.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+############################################################
+HOSTNAME=$(hostname)
+date_time=$(date +'%Y-%m-%d_%H-%M-%S')
+file_name=logs_info.txt
+############################################################
+if [[ $HOSTNAME == "server-1" ]]; then
+	# Array with ip
+	neighbors_ip=("192.168.56.102" "192.168.56.103")
+elif [[ $HOSTNAME == "server-2" ]]; then
+	# Array with ip
+	neighbors_ip=("192.168.56.101" "192.168.56.103")
+elif [[ $HOSTNAME == "server-3" ]]; then 
+	# Array with ip
+	neighbors_ip=("192.168.56.101" "192.168.56.102")
+fi
+############################################################
+for i in "${neighbors_ip[@]}"; do
+	# Put the log' info into the file
+	file_content="- Created by: $HOSTNAME
+- Date-Time: $date_time
+#======================================="
+
+	echo "$file_content" | ssh sftpuser@$i "cat >> /home/sftpuser/$file_name"
+done
+############################################################
