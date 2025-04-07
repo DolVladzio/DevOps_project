5f58e7743d7532f601e0935c0cd24fd046d417cd
DolVladzio <dolynkavladzio@gmail.com>
2025-04-07 17:35:20 +0000

    Updated code; - Parses log's file on each vm and outputs in th econsole

--- HW_02/python/app.py
+++ HW_02/python/app.py
@@ -5,29 +5,40 @@ import os
 
 # app = Flask(__name__)
 
-vm_ip = "192.168.56.102"
+# vm_ip = "192.168.56.102"
+vm_ipc = [
+    "192.168.56.101",
+    "192.168.56.102",
+    "192.168.56.103"
+]
+
 vm_username = "sftpuser"
 private_key_path = os.path.expanduser("~/.ssh/id_rsa")
 logs_file_path = "/home/sftpuser/logs_info.txt"
 ############################################################
 try:
-    # Connecting
+    # Connecting to the vm
     key = paramiko.RSAKey.from_private_key_file(private_key_path)
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect(hostname=vm_ip, username=vm_username, pkey=key)
-    ############################################################
-    # Reading logs
-    cmd = f"cat {logs_file_path}"
-    stdin, stdout, stderr = ssh.exec_command(cmd)
 
-    error_output = stderr.read().decode().strip()
+    for ip in vm_ipc:
+        ssh.connect(hostname=ip, username=vm_username, pkey=key)
+        # Reading logs
+        cmd = f"hostname; cat {logs_file_path}"
 
-    if error_output:
-        print("- Error:", error_output)
-    else:
-        for line in stdout:
-            print(line.strip())
+        stdin, stdout, stderr = ssh.exec_command(cmd)
+
+        error_output = stderr.read().decode().strip()
+
+        if error_output:
+            print("- Error:", error_output)
+        else:
+            for line in stdout:
+                print(line.strip())
+
+            print("##########################################\n")
+############################################################
 except Exception as e:
     print(f'- Something went wrong :( {e}')
 finally:
@@ -36,6 +47,5 @@ finally:
     print("- The end")
 ############################################################
 # @app.route("/")
-# def showItems():
-#     return "<br>".join(output_logs)
+# def showLogs():
 ############################################################
