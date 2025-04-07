5c0eb20ea9397bef205c35de9bde4727093e90f4
DolVladzio <dolynkavladzio@gmail.com>
2025-04-07 17:11:46 +0000

    Updated code

--- HW_02/bash/config.sh
+++ HW_02/bash/config.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 ############################################################
-# Install OpenSSH
+# Installing OpenSSH
 sudo apt update && \
-	sudo apt install -y openssh-server rkhunter --no-install-recommends
+sudo apt install -y openssh-server rkhunter --no-install-recommends
 
-# Create the SFTP user if not exists
+# Creating the SFTP user if not exists
 id -u sftpuser &>/dev/null || useradd -m -s /bin/bash sftpuser
 
-# Set up .ssh and authorized_keys
+# Setting up .ssh and authorized_keys
 mkdir -p /home/sftpuser/.ssh
 chmod 700 /home/sftpuser/.ssh
 ############################################################
-# Check if the public/private keys exist
+# Checking if the public/private keys exist
 if [[ -f /vagrant/ssh/id_rsa.pub && -f /vagrant/ssh/id_rsa ]]; then
-	# Add the public key to the main path
+	# Adding the public key to the main path
 	cat /vagrant/ssh/id_rsa.pub >> /home/sftpuser/.ssh/authorized_keys
 	chmod 600 /home/sftpuser/.ssh/authorized_keys
 	chown -R sftpuser:sftpuser /home/sftpuser/.ssh
 
-	# Add the private key to the main path
+	# Adding the private key to the main path
 	cat /vagrant/ssh/id_rsa >> /home/sftpuser/.ssh/id_rsa
 	chmod 600 /home/sftpuser/.ssh/id_rsa
 	chown -R sftpuser:sftpuser /home/sftpuser/.ssh
@@ -26,17 +26,29 @@ else
 	echo "- The public or private keys weren't found at /vagrant/ssh/"
 fi
 ############################################################
-# Check if the file exists
+# Checking if the file exists
 if [[ -f /vagrant/bash/sent_logs.sh ]]; then
-	# Add the file to the main path
+	# Adding the file to the main path
 	cp /vagrant/bash/sent_logs.sh /home/sftpuser/sent_logs.sh
 	chmod +x /home/sftpuser/sent_logs.sh
 	chown -R sftpuser:sftpuser /home/sftpuser/sent_logs.sh
-	
-	# Configure to launch the file every 5m
+
+	# Configuring to launch the file every 5m
 	echo "*/5 * * * * /home/sftpuser/sent_logs.sh" | crontab -u sftpuser -
+	# Launching file
+	~/home/sftpuser/sent_logs.sh
 else
 	echo "- The file 'sent_logs.sh' wasn't found at /vagrant/bash/sent_logs.sh"
+fi
+############################################################
+# Checking if the file exists
+if [[ -f /vagrant/python/app.py ]]; then
+	# Adding the file to the main path
+	cp -r /vagrant/python/ /home/sftpuser/python
+	chmod +x /home/sftpuser/python
+	chown -R sftpuser:sftpuser /home/sftpuser/python
+else
+	echo "- The dir 'python' wasn't found at /vagrant/python/"
 fi
 ############################################################
 rkhunter --update
