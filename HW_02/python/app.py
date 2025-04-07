0b3b9cdd11b5148d0a9a5be7e3622602c2fd5429
DolVladzio <dolynkavladzio@gmail.com>
2025-04-07 18:07:00 +0000

    Added code for the PostgreSQL; Updated code

--- HW_02/python/app.py
+++ HW_02/python/app.py
@@ -2,10 +2,42 @@
 # from flask import Flask
 import paramiko
 import os
+import psycopg2
+from psycopg2 import sql
 
 # app = Flask(__name__)
 
-# vm_ip = "192.168.56.102"
+# DB #######################################################
+db_name="postgres"
+db_host = '127.0.0.1'
+db_admin_user = 'devops'
+db_admin_password = 'devops'
+
+try:
+    # Connecting to the DB
+    connection = psycopg2.connect(
+        dbname=f"{db_name}", 
+        host=f"{db_host}", 
+        user=f"{db_admin_user}", 
+        password=f"{db_admin_password}"
+    )
+
+    # Create a new database and user
+    with connection.cursor() as cursor:
+        cursor.execute(f"CREATE DATABASE {db_name};")
+        print(f"Database {db_name} was created successfully.")
+
+        cursor.execute(f"CREATE USER {db_admin_user} WITH PASSWORD {db_admin_password};")
+        print(f"User {db_admin_user} created successfully.")
+        
+        cursor.execute(f"GRANT ALL PRIVILEGES ON DATABASE {db_name} TO {db_admin_user};")
+        print(f"Privileges granted to {db_admin_user}.")
+
+    print("Database setup completed.")
+
+except Exception as e:
+    print(f'- Something went wrong :( {e}')
+# VM #######################################################
 vm_ipc = [
     "192.168.56.101",
     "192.168.56.102",
@@ -15,7 +47,7 @@ vm_ipc = [
 vm_username = "sftpuser"
 private_key_path = os.path.expanduser("~/.ssh/id_rsa")
 logs_file_path = "/home/sftpuser/logs_info.txt"
-############################################################
+
 try:
     # Connecting to the vm
     key = paramiko.RSAKey.from_private_key_file(private_key_path)
@@ -38,13 +70,17 @@ try:
                 print(line.strip())
 
             print("##########################################\n")
-############################################################
+
 except Exception as e:
     print(f'- Something went wrong :( {e}')
 finally:
     if ssh:
         ssh.close()
     print("- The end")
+
+    # Close the connection to PostgreSQL
+    connection.commit()
+    connection.close()
 ############################################################
 # @app.route("/")
 # def showLogs():
