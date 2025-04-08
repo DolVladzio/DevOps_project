bcd2c297c749403c264bd5381d57f9fe067bd41d
DolVladzio <dolynkavladzio@gmail.com>
2025-04-08 11:40:03 +0000

    Updated code to connect to the DB and create the table 'LogsDatabase'

--- HW_02/app/python/app.py
+++ HW_02/app/python/app.py
@@ -3,114 +3,122 @@
 import paramiko
 import os
 import re
+import datetime
+import sys
 import psycopg2
 from psycopg2 import sql
 
 # app = Flask(__name__)
+############################################################
+def createTable():
+    db_name = "LogsDatabase"
 
-# DB #######################################################
-# db_name = "Logs"
-# db_host = '127.0.0.1'
-# db_admin_user = 'postgres'
-# db_admin_password = 'postgres' 
+    try:
+        print("- Connecting to the database...")
+        db_connection = psycopg2.connect(
+            dbname="postgres", 
+            user="postgres", 
+            password="devops", 
+            host="127.0.0.1"
+        )
+        print("- Connected to the database.\n")
 
-# try:
-#     # Connecting to the 'postgres' database as a superuser
-#     connection = psycopg2.connect(
-#         dbname="postgres",
-#         host=db_host,
-#         user=db_admin_user,
-#         password=db_admin_password
-#     )
+        cursor = db_connection.cursor()
+        db_connection.autocommit = True
+        
+        try:
+            # Create a new database
+            print(f"- Creating database '{db_name}'...")
+            sql = f"CREATE DATABASE {db_name};"
+            cursor.execute(sql)
+            print(f"- Database '{db_name}' created successfully.\n")
+        except Exception as e:
+            print(f"- Database '{db_name}' already exists.\n")
 
-#     # Avoiding the "cannot run inside a transaction block" error
-#     connection.autocommit = True
+    except Exception as e:
+        print(f'- An exception occurred :(\n{e}')
+    finally:
+        if db_connection:
+            cursor.close()
+            db_connection.close()
+            print("- DB's connection closed.")
+############################################################
+# def insterLogs():
+#     vm_ipc = [
+#         "192.168.56.101",
+#         "192.168.56.102",
+#         "192.168.56.103"
+#     ]
 
-#     print(f"Successfully connected to the database 'postgres'.")
+#     vm_username = "sftpuser"
+#     private_key_path = os.path.expanduser("~/.ssh/id_rsa")
+#     logs_file_path = "/home/sftpuser/logs_info.txt"
 
-#     # Checking/Creating new database and user
+#     # Regular expression pattern to match date_time and text
+#     regex = r"(?P<date_time>\d{4}-\d{2}-\d{2}_\d{2}-\d{2}-\d{2}) \| (?P<text>.+)"
+
 #     try:
-#         with connection.cursor() as cursor:
-#             # Checking/Creating the new database 'Logs'
-#             cursor.execute(f"CREATE DATABASE {db_name};")
-#             print(f"Database {db_name} was created successfully.")
+#         # Reading the private key
+#         key = paramiko.RSAKey.from_private_key_file(private_key_path)
+#         ssh = paramiko.SSHClient()
+#         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-#             # Checking/Creating a new user 'devops' with password 'devops'
-#             cursor.execute(f"CREATE USER {db_admin_user} WITH PASSWORD '{db_admin_password}';")
-#             print(f"User {db_admin_user} created successfully.")
+#         # Connecting to the vm
+#         for ip in vm_ipc:
+#             print(f"----------------------------\n- Connecting to {ip}...")
+#             ssh.connect(hostname=ip, username=vm_username, pkey=key)
+#             print(f"- Connected to {ip}")
 
-#             # Grant privileges to the 'devops' user for the 'Logs' database
-#             cursor.execute(f"GRANT ALL PRIVILEGES ON DATABASE {db_name} TO {db_admin_user};")
-#             print(f"Privileges granted to {db_admin_user} on database {db_name}.")
+#             # Use SFTP to retrieve the logs file data
+#             sftp = ssh.open_sftp()
+#             print("\n- Logs retrieving...")
 
-#             print("Database setup completed.")
-#     except:
-#         print(f"- The user {db_admin_user}' already exists\n- The  database {db_name} already exists\n")
+#             with sftp.file(logs_file_path, "r") as remote_file:
+#                 log_data = remote_file.read().decode("utf-8")
 
-# except Exception as e:
-#     print(f'- Something went wrong :(\n{e}')
+#             sftp.close()
+#             print(f"\n- Logs retrieved successfully from {ip}.")
 
-# finally:
-#     if 'connection' in locals() and connection:
-#         connection.close()
-#         print("Connection closed.")
-# VM #######################################################
-vm_ipc = [
-    "192.168.56.101",
-    "192.168.56.102",
-    "192.168.56.103"
-]
+#             # Extracting data from the file using regex
+#             for match in re.finditer(regex, log_data):
+#                 date_time = match.group("date_time")
+#                 text = match.group("text")
 
-vm_username = "sftpuser"
-private_key_path = os.path.expanduser("~/.ssh/id_rsa")
-logs_file_path = "/home/sftpuser/logs_info.txt"
+#                 # Convert the date_time format to a valid timestamp format
+#                 date_time = date_time.replace('_', ' ')
 
-# Regular expression pattern to match date_time and text
-regex = r"(?P<date_time>\d{4}-\d{2}-\d{2}_\d{2}-\d{2}-\d{2}) \| (?P<text>.+)"
+#                 try:
+#                     print("- Inserting logs into the database...")
+#                     # Convert the string to a Python datetime object
+#                     date_time_obj = datetime.datetime.strptime(date_time, "%Y-%m-%d %H-%M-%S")
 
-try:
-    # Reading the private key
-    key = paramiko.RSAKey.from_private_key_file(private_key_path)
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+#                     with connection.cursor() as cursor:
+#                         insert_query = sql.SQL("INSERT INTO Logs (date_time, text) VALUES (%s, %s);")
+#                         cursor.execute(insert_query, (date_time_obj, text))
+#                 except Exception as e:
+#                     print(f"- Failed to insert logs into the database :(\n{e}")
 
-    # Connecting to the vm
-    for ip in vm_ipc:
-        print(f"- Connecting to {ip}...")
-        ssh.connect(hostname=ip, username=vm_username, pkey=key)
-        print(f"- Connected to {ip}")
+#             print(f"- Logs successfully inserted into the database")
 
-        # Use SFTP to retrieve the logs file
-        sftp = ssh.open_sftp()
-        print("- Logs retrieving...\n")
+#     except Exception as e:
+#         print(f'- Something went wrong :( {e}')
+#     finally:
+#         if ssh:
+#             ssh.close()
+#             print("- SSH connection closed.")
 
-        with sftp.file(logs_file_path, "r") as remote_file:
-            log_data = remote_file.read().decode("utf-8")
+#         # Close the connection to PostgreSQL
+#         if 'connection' in locals() and connection:
+#             connection.close()
+#             print("- DB's connection closed.")
 
-        sftp.close()
-
-        # Extracting data from the file using regex
-        for match in re.finditer(regex, log_data):
-            date_time = match.group("date_time")
-            text = match.group("text")
-            print(f"{date_time} | {text}")
-
-        print(f"\n- Logs retrieved successfully from {ip}.\n")
-
-except Exception as e:
-    print(f'- Something went wrong :( {e}')
-finally:
-    if ssh:
-        ssh.close()
-        print("- SSH connection closed.")
-
-    # Close the connection to PostgreSQL
-    # if 'connection' in locals() and connection:
-    #     connection.close()
-    #     print("- DB's connection closed.")
-
-    print("- The end")
+#         print("- The end")
 ############################################################
 # @app.route("/")
 # def showLogs():
+############################################################
+if __name__ == "__main__":
+    # insterLogs()
+
+    createTable()
 ############################################################
