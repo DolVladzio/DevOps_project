9f7581c1523d41033a51e6863bebec727f425764
DolVladzio <dolynkavladzio@gmail.com>
2025-04-09 10:33:25 +0000

    Updated code

--- HW_02/app/python/app.py
+++ HW_02/app/python/app.py
@@ -15,6 +15,25 @@ db_password = "devops"
 db_host = "127.0.0.1"
 db_table_name = "logs"
 ##############################################################################
+def DBConnection():
+    try:
+        print(f"- Connecting to the database {db_name}...")
+        db_connection = psycopg2.connect(
+            dbname=db_name,
+            user=db_username,
+            password=db_password,
+            host=db_host
+        )
+        cursor = db_connection.cursor()
+        db_connection.autocommit = True
+
+        print(f"- Connected to the database: {db_name}\n")
+        return db_connection, cursor 
+    
+    except Exception as e:
+        print(f"Failed to connect to the database: {e}")
+        raise
+##############################################################################
 def createDB():
     try:
         print("-----------------------------\n- Connecting to the database...")
@@ -45,25 +64,6 @@ def createDB():
             db_connection.close()
             print("- DB's connection closed.\n-----------------------------")
 ##############################################################################
-def DBConnection():
-    try:
-        print(f"- Connecting to the database {db_name}...")
-        db_connection = psycopg2.connect(
-            dbname=db_name,
-            user=db_username,
-            password=db_password,
-            host=db_host
-        )
-        cursor = db_connection.cursor()
-        db_connection.autocommit = True
-
-        print(f"- Connected to the database: {db_name}")
-        return db_connection, cursor 
-    
-    except Exception as e:
-        print(f"Failed to connect to the database: {e}")
-        raise
-##############################################################################
 def createTable():
     try:
         db_connection, cursor = DBConnection()
@@ -85,74 +85,88 @@ def createTable():
         if db_connection:
             cursor.close()
             db_connection.close()
-            print("- DB's connection closed")
-##############################################################################
-def insertLogs():
-    vm_ipc = [
-        "192.168.56.101",
-        "192.168.56.102",
-        "192.168.56.103"
-    ]
-
-    vm_username = "sftpuser"
-    private_key_path = os.path.expanduser("~/.ssh/id_rsa")
-    logs_file_path = "/home/sftpuser/logs_info.txt"
-    regex = r"(?P<date_time>\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}) \| (?P<text>.+)"
-    ssh = None
-    db_connection = None
-
-    try:
-        key = paramiko.RSAKey.from_private_key_file(private_key_path)
-        ssh = paramiko.SSHClient()
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
-        db_connection, cursor = DBConnection()
-
-        for ip in vm_ipc:
-            print(f"----------------------------\n- Connecting to {ip}...")
-            ssh.connect(hostname=ip, username=vm_username, pkey=key)
-            print(f"- Connected to {ip}\n")
-
-            sftp = ssh.open_sftp()
-            with sftp.file(logs_file_path, "r") as remote_file:
-                log_data = remote_file.read().decode("utf-8")
-
-                for match in re.finditer(regex, log_data):
-                    date_time = match.group("date_time").replace('_', ' ')
-                    text = match.group("text")
-
-                    try:
-                        insert_query = sql.SQL("INSERT INTO {table} (date_time, text) VALUES (%s, %s);").format(
-                            table=sql.Identifier(db_table_name)
-                        )
-                        cursor.execute(insert_query, (date_time, text))
-                        print(f"- Log inserted: {date_time} | {text}")
-                    except Exception as e:
-                        print(f"- Failed to insert logs for date_time: {date_time}, text: {text}")
-                        print(f"  Error: {e}")
-
-                print(f"\n- Logs retrieved successfully from {ip}")
-
-        sftp.close()
-
-    except Exception as e:
-        print(f"- An exception occurred :(\n{e}")
-    
-    finally:
-        # Close the SSH connection
-        if ssh is not None:
-            ssh.close()
-            print("----------------------------\n- SSH connection closed.")
-        # Close the database connection
-        if db_connection:
-            cursor.close()
-            db_connection.close()
             print("- DB's connection closed.\n-----------------------------")
+##############################################################################
+# def insertLogs():
+#     vm_ipc = [
+#         "192.168.56.101",
+#         "192.168.56.102",
+#         "192.168.56.103"
+#     ]
+
+#     vm_username = "sftpuser"
+#     private_key_path = os.path.expanduser("~/.ssh/id_rsa")
+#     logs_file_path = "/home/sftpuser/logs_info.txt"
+#     regex = r"(?P<date_time>\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}) \| (?P<text>.+)"
+#     ssh = None
+#     db_connection = None
+
+#     try:
+#         key = paramiko.RSAKey.from_private_key_file(private_key_path)
+#         ssh = paramiko.SSHClient()
+#         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+#         db_connection, cursor = DBConnection()
+
+#         for ip in vm_ipc:
+#             print(f"----------------------------\n- Connecting to {ip}...")
+#             ssh.connect(hostname=ip, username=vm_username, pkey=key)
+#             print(f"- Connected to {ip}\n")
+
+#             command = "hostname"
+#             # Execute the command to get vm name
+#             stdin, stdout, stderr = ssh.exec_command(command)
+            
+#             # Read command output and error
+#             output = stdout.read().decode('utf-8')
+#             error = stderr.read().decode('utf-8')
+            
+#             if output:
+#                 print(f"Command Output: {output}")
+#             if error:
+#                 print(f"Command Error: {error}")
+
+#             sftp = ssh.open_sftp()
+#             with sftp.file(logs_file_path, "r") as remote_file:
+#                 log_data = remote_file.read().decode("utf-8")
+
+#                 for match in re.finditer(regex, log_data):
+#                     date_time = match.group("date_time").replace('_', ' ')
+#                     text = match.group("text")
+
+#                     try:
+#                         insert_query = sql.SQL("INSERT INTO {table} (date_time, text) VALUES (%s, %s);").format(
+#                             table=sql.Identifier(db_table_name)
+#                         )
+#                         cursor.execute(insert_query, (date_time, text))
+#                         print(f"- Log inserted: {date_time} | {text}")
+#                     except Exception as e:
+#                         print(f"- Failed to insert logs for date_time: {date_time}, text: {text}")
+#                         print(f"  Error: {e}")
+
+#                 print(f"\n- Logs retrieved successfully from {ip}")
+
+#         sftp.close()
+
+#     except Exception as e:
+#         print(f"- An exception occurred :(\n{e}")
+    
+#     finally:
+#         # Close the SSH connection
+#         if ssh is not None:
+#             ssh.close()
+#             print("----------------------------\n- SSH connection closed.")
+#         # Close the database connection
+#         if db_connection:
+#             cursor.close()
+#             db_connection.close()
+#             print("- DB's connection closed.\n-----------------------------")
 ##############################################################################
 @app.route("/")
 def showLogs():
     db_connection = None
     cursor = None
+
     try:
         # Connect to the database
         db_connection, cursor = DBConnection()
@@ -175,12 +189,4 @@ def showLogs():
         if db_connection:
             cursor.close()
             db_connection.close()
-##############################################################################
-if __name__ == "__main__":
-    createDB()
-    createTable()
-    insertLogs()
-    print("\n- All tasks completed successfully.")
-
-    app.run(debug=True)
 ##############################################################################
