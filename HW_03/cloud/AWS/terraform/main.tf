f3559d0b765e1866897753f97327cc3f8ec24ee3
DolVladzio <dolynkavladzio@gmail.com>
2025-04-22 13:30:40 +0000

    Updated code to put public shh key into the instance

--- HW_03/cloud/AWS/terraform/main.tf
+++ HW_03/cloud/AWS/terraform/main.tf
@@ -1,7 +1,7 @@
 #########################################################################
 # Resources 
 #########################################################################
-resource "aws_vpc" "example" {
+resource "aws_vpc" "HW_03_vpc" {
 	cidr_block           = "10.0.0.0/16"
 	enable_dns_support   = true
 	enable_dns_hostnames = true
@@ -11,8 +11,8 @@ resource "aws_vpc" "example" {
 	}
 	}
 
-	resource "aws_subnet" "example" {
-	vpc_id            = aws_vpc.example.id
+	resource "aws_subnet" "HW_03_subnet" {
+	vpc_id            = aws_vpc.HW_03_vpc.id
 	cidr_block        = "10.0.1.0/24"
 	availability_zone = "eu-west-1a"
 
@@ -21,10 +21,10 @@ resource "aws_vpc" "example" {
 	}
 }
 #########################################################################
-resource "aws_security_group" "example" {
+resource "aws_security_group" "HW_03_secutiry_group" {
 	name        = "example-sg"
 	description = "Allow SSH and HTTP"
-	vpc_id      = aws_vpc.example.id
+	vpc_id      = aws_vpc.HW_03_vpc.id
 
 	ingress {
 		from_port   = 22
@@ -48,10 +48,16 @@ resource "aws_security_group" "example" {
 	}
 }
 #########################################################################
-resource "aws_instance" "example" {
+resource "aws_key_pair" "HW_03_key_pair" {
+	key_name   = var.KEY_PAIR_NAME
+	public_key = file("${var.KEY_PAIR_NAME}.pub")
+}
+#########################################################################
+resource "aws_instance" "HW_03_instance" {
 	ami           = var.AMI_ID
 	instance_type = var.INSTANCE_TYPE
-	subnet_id     = aws_subnet.example.id
+	key_name	  = aws_key_pair.HW_03_key_pair.id
+	subnet_id     = aws_subnet.HW_03_subnet.id
 	security_groups = [aws_security_group.example.id]
 
 	tags = {
--- HW_03/cloud/AWS/terraform/variables.tf
+++ HW_03/cloud/AWS/terraform/variables.tf
@@ -11,13 +11,18 @@ variable "DEV" {
 	default     = "HW_03_instance"
 }
 #----------------------------------------------------
-variable "INSTANCE_TYPE" {
-	description = "AWS instance's type"
-	default     = "t2.micro"
+variable "KEY_PAIR_NAME" {
+	description = "SSH key's name"
+	default     = "aws_instance"
 }
 #----------------------------------------------------
 variable "AMI_ID" {
 	description = "The AMI ID for the EC2 instance"
 	default     = "ami-0f0c3baa60262d5b9"
+}
+#----------------------------------------------------
+variable "INSTANCE_TYPE" {
+	description = "AWS instance's type"
+	default     = "t2.micro"
 }
 #----------------------------------------------------
