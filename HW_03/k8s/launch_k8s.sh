06479cd340ff4731acda06d30d8580b51c9f1e0c
DolVladzio <dolynkavladzio@gmail.com>
2025-05-03 17:58:38 +0000

    Updated code

--- HW_03/k8s/launch_k8s.sh
+++ HW_03/k8s/launch_k8s.sh
@@ -3,7 +3,10 @@
 kubectl apply -f app-config.yml
 # == Setup services =========================
 kubectl apply -f services/
-# == Setup deployments ======================
-kubectl apply -f deployments/
+# == Setup redis && postgres deployments ====
+kubectl apply -f deployments/redis-deployment.yml && kubectl apply -f deployments/postgres-deployment.yml
 # == Wait for pods =======================
 kubectl wait pods --all --for condition=Ready
+# == Setup frontend && backend deployments ==
+kubectl apply -f deployments/backend-deployment.yml && kubectl apply -f deployments/frontend-deployment.yml
+
