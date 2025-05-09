76571bd85bc11fb69efb7ce1992d1a144b46a640
DolVladzio <dolynkavladzio@gmail.com>
2025-05-09 18:27:59 +0000

    Updated the main files

--- HW_03/cloud/AWS/terraform/main.tf
+++ HW_03/cloud/AWS/terraform/main.tf
@@ -1,102 +1,26 @@
 #########################################################################
 # Resources
 #########################################################################
-resource "aws_vpc" "HW_03_vpc" {
-	cidr_block           = "10.0.0.0/16"
-	enable_dns_support   = true
-	enable_dns_hostnames = true
-
-	tags = {
-		Name = "${var.DEV}_vpc"
-	}
-	}
-
-	resource "aws_subnet" "HW_03_subnet" {
-	vpc_id            = aws_vpc.HW_03_vpc.id
-	cidr_block        = "10.0.1.0/24"
-	availability_zone = "eu-west-1a"
-	map_public_ip_on_launch = true
-
-	tags = {
-		Name = "${var.DEV}_subnet"
-	}
+locals {
+	config = jsondecode(file("${path.module}/../../config.json"))
+	region = local.config.project.state_bucket_location_aws
 }
 #########################################################################
-resource "aws_security_group" "HW_03_security_group" {
-	name        = "example-sg"
-	description = "Allow SSH and HTTP"
-	vpc_id      = aws_vpc.HW_03_vpc.id
-
-	ingress {
-		from_port   = 22
-		to_port     = 22
-		protocol    = "tcp"
-		cidr_blocks = ["0.0.0.0/0"]
-	}
-
-	ingress {
-		from_port   = 80
-		to_port     = 80
-		protocol    = "tcp"
-		cidr_blocks = ["0.0.0.0/0"]
-	}
-
-	ingress {
-                from_port   = 8080
-                to_port     = 8080
-                protocol    = "tcp"
-                cidr_blocks = ["0.0.0.0/0"]
-        }
-
-	egress {
-		from_port   = 0
-		to_port     = 0
-		protocol    = "-1"
-		cidr_blocks = ["0.0.0.0/0"]
-	}
+module "network" {
+	source = "./modules/network"
+	region = local.region
+	vpcs = local.config.network
 }
 #########################################################################
-resource "aws_key_pair" "HW_03_key_pair" {
-	key_name   = var.SSH_KEY_NAME
-	public_key = file("~/.ssh/terraform-aws.pem.pub")
+module "security_groups" {
+	source = "./modules/security_groups"
 }
 #########################################################################
-resource "aws_internet_gateway" "HW_03_igw" {
-	vpc_id = aws_vpc.HW_03_vpc.id
-
-	tags = {
-                Name = "${var.DEV}_internet_gateway"
-        }
+module "vms" {
+	source = "./modules/vms"
 }
 #########################################################################
-resource "aws_route_table" "HW_03_route_table" {
-	vpc_id = aws_vpc.HW_03_vpc.id
-
-	route {
-		cidr_block = "0.0.0.0/0"
-		gateway_id = aws_internet_gateway.HW_03_igw.id
-	}
-
-	tags = {
-                Name = "${var.DEV}_route_table"
-        }
-}
-#########################################################################
-resource "aws_route_table_association" "HW_03_route_table_association" {
-	subnet_id      = aws_subnet.HW_03_subnet.id
-	route_table_id = aws_route_table.HW_03_route_table.id
-}
-#########################################################################
-resource "aws_instance" "HW_03_instance" {
-	ami           = var.AMI_ID
-	instance_type = var.INSTANCE_TYPE
-	subnet_id     = aws_subnet.HW_03_subnet.id
-	key_name      = aws_key_pair.HW_03_key_pair.key_name
-	security_groups = [aws_security_group.HW_03_security_group.id]
-	associate_public_ip_address = true
-
-	tags = {
-		Name = "${var.DEV}_instance"
-	}
+module "db" {
+	source = "./modules/database"
 }
 #########################################################################
