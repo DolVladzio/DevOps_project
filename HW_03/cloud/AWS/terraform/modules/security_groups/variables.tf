c9a150fb455c8f0c12aca59393af7b4e2446a9c7
DolVladzio <dolynkavladzio@gmail.com>
2025-05-10 16:09:00 +0000

    Deleted all data

--- HW_03/cloud/AWS/terraform/modules/security_groups/variables.tf
+++ HW_03/cloud/AWS/terraform/modules/security_groups/variables.tf
@@ -1,28 +1,4 @@
 #########################################################################
 # Variables
 #########################################################################
-variable "AWS_REGION" {
-	description = "AWS region"
-	default     = "eu-west-1"
-}
-#----------------------------------------------------
-variable "DEV" {
-	description = "AWS instance's name"
-	default     = "HW_03"
-}
-#----------------------------------------------------
-variable "SSH_KEY_NAME" {
-	description = "SSH key's name"
-	default     = "terraform-aws"
-}
-#----------------------------------------------------
-variable "AMI_ID" {
-	description = "The AMI ID for the EC2 instance"
-	default     = "ami-0f0c3baa60262d5b9"
-}
-#----------------------------------------------------
-variable "INSTANCE_TYPE" {
-	description = "AWS instance's type"
-	default     = "t2.micro"
-}
 #----------------------------------------------------
