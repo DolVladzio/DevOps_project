4bdb1db04b2b50e6e3f094b439627f1226015042
Vladzio <dolynkavladzio@gmail.com>
2025-05-15 12:17:04 +0000

    Added a module network and all necessary env

--- HW_03/cloud/GCP/terraform/main.tf
+++ HW_03/cloud/GCP/terraform/main.tf
@@ -7,3 +7,11 @@ locals {
 	region = local.config.project.repository_location_gcp
 }
 #########################################################################
+module "network" {
+	source = "./modules/network"
+	region = local.region
+	acls = local.config.networks
+	network = local.config.network
+	security_groups = local.config.security_groups
+}
+#########################################################################
