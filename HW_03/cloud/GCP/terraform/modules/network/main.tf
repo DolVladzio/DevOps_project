399afd2e23c5739b05dd4d75ddb766bdb051e651
Vladzio <dolynkavladzio@gmail.com>
2025-05-15 13:10:44 +0000

    Added the resource 'google_compute_firewall'

--- HW_03/cloud/GCP/terraform/modules/network/main.tf
+++ HW_03/cloud/GCP/terraform/modules/network/main.tf
@@ -37,3 +37,35 @@ resource "google_compute_subnetwork" "subnet" {
 	network       = google_compute_network.vpc[each.value.network_name].id
 }
 #########################################################################
+resource "google_compute_firewall" "default" {
+	for_each = {
+		for sg in var.security_groups : sg.name => sg
+		if length(sg.ingress) > 0
+	}
+
+	name    = each.value.name
+	network = google_compute_network.vpc[each.value.vpc].self_link
+	target_tags = each.value.attach_to
+	description = each.value.description
+
+	dynamic "allow" {
+		for_each = each.value.ingress
+		content {
+			protocol = allow.value.protocol
+			ports    = [tostring(allow.value.port)]
+		}
+	}
+	# Handle source ranges properly - either use CIDR from ACLs or default to 0.0.0.0/0
+	source_ranges = distinct(flatten([
+		for rule in each.value.ingress : 
+		contains(keys(local.acls_map), rule.source) ? [local.acls_map[rule.source]] : ["0.0.0.0/0"]
+		if !contains(keys(local.sg_to_instances_map), rule.source)
+	]))
+	
+	# Handle source tags when source is another security group
+	source_tags = distinct(flatten([
+		for rule in each.value.ingress :
+		contains(keys(local.sg_to_instances_map), rule.source) ? local.sg_to_instances_map[rule.source] : []
+	]))
+}
+#########################################################################
