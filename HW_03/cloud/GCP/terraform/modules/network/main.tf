f1f56ebd468f7501533be9174ecdaaf8f72fb042
Vladzio <dolynkavladzio@gmail.com>
2025-05-15 12:17:43 +0000

    Added the resource 'google_compute_network'

--- HW_03/cloud/GCP/terraform/modules/network/main.tf
+++ HW_03/cloud/GCP/terraform/modules/network/main.tf
@@ -1,5 +1,20 @@
 #########################################################################
 # RESOURCES
 #########################################################################
+locals {
+	# map ACL name → cidr
+	acls_map = { for a in var.acls : a.name => a.cidr }
 
+	# Create a map of VPC name to VPC for easier lookup
+	vpcs_map = { for vpc in var.network : vpc.name => vpc }
+
+	# Create a map of security group name to the instances it's attached to
+	sg_to_instances_map = { for sg in var.security_groups : sg.name => sg.attach_to }
+}
+#########################################################################
+resource "google_compute_network" "vpc" {
+	for_each                = local.vpcs_map
+	name                    = "${each.key}-vpc"
+	auto_create_subnetworks = false
+}
 #########################################################################
