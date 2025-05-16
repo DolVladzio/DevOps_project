13cbaad96dc7cd28b1fe1f7349d9ccd3d66d3f64
Vladzio <dolynkavladzio@gmail.com>
2025-05-16 11:42:34 +0000

    Added two env 'db_pass' and 'db_username' for the module 'db_instance'; Added two data 'google_secret_manager_secret_version_access'('db_username' and 'db_username')

--- HW_03/cloud/GCP/terraform/main.tf
+++ HW_03/cloud/GCP/terraform/main.tf
@@ -1,14 +1,29 @@
 #########################################################################
 # MODULES
 #########################################################################
+data "google_secret_manager_secret_version_access" "db_username" {
+	secret  = "db_username"
+	version = "latest"
+}
+#########################################################################
+data "google_secret_manager_secret_version_access" "db_password" {
+	secret  = "db_pass"
+	version = "latest"
+}
+#########################################################################
 locals {
 	config = jsondecode(file("${path.module}/../../config.json"))
+
+	region = local.config.project.repository_location_gcp
+
+	db_username = data.google_secret_manager_secret_version_access.db_username.secret
+	db_password = data.google_secret_manager_secret_version_access.db_password.secret
 }
 #########################################################################
 module "network" {
 	source = "./modules/network"
 	project_id = var.project_id
-	region = local.config.project.repository_location_gcp
+	region = local.region
 	acls = local.config.networks
 	network = local.config.network
 	security_groups = local.config.security_groups
@@ -21,7 +36,7 @@ module "vm" {
 	vm_instances = local.config.security_groups
 	subnet_self_links_map = module.network.subnet_self_links_by_name
 	project_os = local.config.project.os
-	
+
 	depends_on = [ module.network ]
 }
 #########################################################################
@@ -32,6 +47,8 @@ module "db_instance" {
 	database = local.config.database
 	private_networks = module.network.vpc_self_links
 	subnet_self_links = module.network.subnet_self_links_by_name
+	db_pass = local.db_password
+	db_username = local.db_username
 	
 	depends_on       = [module.network]
 }
