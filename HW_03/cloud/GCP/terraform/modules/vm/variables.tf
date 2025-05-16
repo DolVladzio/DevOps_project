8a9f14cbf7b61de07c3ca77c96f308aaa626ba0d
Vladzio <dolynkavladzio@gmail.com>
2025-05-16 10:52:07 +0000

    Added variables for the module 'vm' based on the json config

---
+++ HW_03/cloud/GCP/terraform/modules/vm/variables.tf
@@ -0,0 +1,24 @@
+#########################################################################
+# VARIABLES
+#########################################################################
+variable "region" {}
+#########################################################################
+variable "project_id" {}
+#########################################################################
+variable "vm_instances" {
+	type = list(object({
+		name = string
+		network = string
+		size = string
+		zone = string
+		subnet = string
+		tags = list(string)
+		public_ip = bool
+		port = number
+		security_groups = optional(list(string), [])
+	}))
+	description = "List of VMs (from config.json)"
+}
+#########################################################################
+variable "subnet_self_links_map" {}
+#########################################################################
