d8c052515d4021e54a79fd7b1451d69150d74b09
Vladzio <dolynkavladzio@gmail.com>
2025-05-16 11:06:17 +0000

    Added outputs for the module 'vm'

--- HW_03/cloud/GCP/terraform/outputs.tf
+++ HW_03/cloud/GCP/terraform/outputs.tf
@@ -4,7 +4,7 @@
 output "GCP_REGION" {
 	value = local.region
 }
-#########################################################################
+### NETWORK #############################################################
 output "subnet_self_links" {
 	value = module.network.subnet_self_links
 }
@@ -16,4 +16,14 @@ output "subnet_self_links_by_name" {
 output "vpc_self_links" {
 	value = module.network.vpc_self_links
 }
+### VM ##################################################################
+output "public_ips" {
+	value = module.vm.public_ips
+}
+#########################################################################
+output "private_ips" {
+	value = module.vm.private_ips
+}
+### DB_INSTANCE #########################################################
+
 #########################################################################
