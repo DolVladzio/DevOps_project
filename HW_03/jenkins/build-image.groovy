54c2ab4b4b93d5ef81b5362a3e59423df87df823
Vladzio <dolynkavladzio@gmail.com>
2025-06-18 11:06:32 +0000

    Deleted a groovy file and added a func into the Jenkinsfile

--- HW_03/jenkins/build-image.groovy
+++
@@ -1,7 +0,0 @@
-def call(String dockerImageName, String dockerfileSource) {
-    dir(dockerfileSource) {
-        echo "- Building the docker image: ${dockerImageName}..."
-        sh "docker build -t ${dockerImageName} ."
-        echo "- The docker image: ${dockerImageName} was built successfully"
-    }
-}
